81edf1b9c7c5d18c4a05e7b769f2c85ca8b0fa4f
Gabriela Urbanová <gabul1990@gmail.com>
2025-05-04 11:48:59 +0200

    pridani confirm na ikony

--- hra.js
+++ hra.js
@@ -26,3 +26,23 @@ const handleClick = (event) => {
 fields.forEach((field) => {
   field.addEventListener('click', handleClick);
 });
+
+const homeBtn = document.querySelector('.ikony__home');
+if (homeBtn) {
+  homeBtn.addEventListener('click', (e) => {
+    const confirmHome = confirm('Opravdu chceš odejít na hlavní stránku?');
+    if (!confirmHome) {
+      e.preventDefault();
+    }
+  });
+}
+
+const restartBtn = document.querySelector('.ikony__restart');
+if (restartBtn) {
+  restartBtn.addEventListener('click', (e) => {
+    const confirmRestart = confirm('Opravdu chceš restartovat hru?');
+    if (!confirmRestart) {
+      e.preventDefault();
+    }
+  });
+}
